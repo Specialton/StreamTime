8d51c172e8050dfe1ca0f61220020b9d2b5dad48
Chris Frisina <github@specialorange.org>
2012-08-24 21:54:57 -0400

    rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StreamTime::Application.config.secret_token = '15b87eb5e4134293178551162d4f4a930349f6c3ea55bb6ac1254e370d46aa96ce0863098ede51c192b5e9399805da9d72719da65a1e63cfaf57e62a203c290e'
