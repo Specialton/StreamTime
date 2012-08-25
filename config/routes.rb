e0ad4a9ed04e01bfe75422ec4ab8ddf6fadcf6a6
Chris Frisina <github@specialorange.org>
2012-08-25 16:21:20 -0400

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 StreamTime::Application.routes.draw do
+  get "home/index"
+
   resources :shift_assignments
 
   resources :shifts
@@ -70,7 +72,7 @@ StreamTime::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
